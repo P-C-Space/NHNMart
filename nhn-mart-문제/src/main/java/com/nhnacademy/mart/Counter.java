06f65b8305f4453a37a7c38df8181400ba35e1a4
jeongwoo <sun62073812@gmail.com>
2023-09-01 16:51:53 +0900

    고객 클래스 구현

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/Counter.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/Counter.java
@@ -2,6 +2,12 @@ package com.nhnacademy.mart;
 
 public class Counter {
 
-    // TODO pay 메서드 구현 (카운터에서 계산 처리 메서드)
+    public static int pay(Basket basket){
+        int sum = 0;
+        for(Food food : basket.getFoods()){
+            sum += food.getPrice();
+        }
+        return sum;
+    }
 
 }
