6476d975c3e5bc94cf253f286e0f731c4dc2c131
jeongwoo <sun62073812@gmail.com>
2023-09-01 16:52:31 +0900

    테스트 코드 작성

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/Customer.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/Customer.java
@@ -2,6 +2,9 @@ package com.nhnacademy.mart;
 
 public class Customer {
 
+    // 고객이 가진 돈
+    private int BuyableMoney;
+
     // 고객 구매 목록
     private final BuyList buyList;
 
@@ -10,6 +13,7 @@ public class Customer {
 
     public Customer(BuyList buyList) {
         this.buyList = buyList;
+        this.BuyableMoney = 20000;
     }
 
     // 장바구니 챙기기
@@ -17,10 +21,29 @@ public class Customer {
         this.basket = basket;
     }
 
-    // TODO pickFoods 메서드 구현
+    public void pickFoods(FoodStand foodStand) {
+
+        // 집고, 삭제
+        for (BuyList.Item item : buyList.getItems()) {             // 구매 리스트 상품들
+            for (int i = 0; i < item.getAmount(); i++) {           // 개수만큼
+                basket.add(foodStand.delete(item.getName()));      // 진열대에서 삭제 후 장바구니 추가
 
 
-    // TODO payTox 메서드 구현
+            }
+        }
+    }
 
+
+
+    public void payTox(Counter counter) {
+        int payPrice = counter.pay(basket);
+        if(BuyableMoney < payPrice){
+            throw new IllegalArgumentException("구매 항목이 소지한 금액을 초과 하였습니다.");
+        }
+        System.out.println("총 가격은 " + payPrice + "원 입니다.");
+        BuyableMoney -= payPrice;
+
+        System.out.println("고객님 결제 후 잔액 : " + BuyableMoney + "원");
+    }
 
 }
