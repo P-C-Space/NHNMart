1f8d1966fdb0a6135ae31e2561cc9ef5df13b3bf
jeongwoo <sun62073812@gmail.com>
2023-09-01 18:25:13 +0900

    정규표현식 예외처리

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/BuyList.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/BuyList.java
@@ -2,17 +2,35 @@ package com.nhnacademy.mart;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BuyList {
-
+    private static final Logger logger = LoggerFactory.getLogger(BuyList.class);
     private final ArrayList<Item> items = new ArrayList<>();
 
-    public void add(String addList){
+    public void add(String addList) {
         StringTokenizer st = new StringTokenizer(addList);
-        while(st.hasMoreTokens()){ // 다음 토큰이 없다면
-            String name = st.nextToken();
-            int amount = Integer.valueOf(st.nextToken());
-            items.add(new Item(name, amount));
+
+        String regExp = "^[0-9]+$";
+
+        while (st.hasMoreTokens()) { // 다음 토큰이 있다면
+            String name = "";
+            String amount = "";
+
+            name = st.nextToken();
+            if (name.matches(regExp)) { // 숫자가 들어온다면
+                logger.warn("잘못된 인자 입력 \"구매품목\"이 입력되어야 합니다.");
+                throw new IllegalArgumentException("잘못된 인자 입력 \"구매품목과\"이 입력되어야 합니다.");
+            }
+            amount = st.nextToken();
+            if (!amount.matches(regExp)) { // 숫자가 들어오지 않는다면
+                logger.warn("잘못된 인자 입력 \"금액\"이 입력되어야 합니다.");
+                throw new IllegalArgumentException("잘못된 인자 입력 \"금액\"이 입력되어야 합니다.");
+            }
+            items.add(new Item(name, Integer.valueOf(amount)));
+
+
         }
     }
 
@@ -25,7 +43,7 @@ public class BuyList {
         private final String name;
         private final int amount;
 
-        public Item(String name, int amount){
+        public Item(String name, int amount) {
             this.name = name;
             this.amount = amount;
         }
