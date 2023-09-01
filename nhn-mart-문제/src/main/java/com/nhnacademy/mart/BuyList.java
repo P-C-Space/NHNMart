112be6968536ba79bcab5e013c7bd8a5b4d64469
jeongwoo <sun62073812@gmail.com>
2023-09-01 16:52:05 +0900

    고객 클래스 구현

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/BuyList.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/BuyList.java
@@ -1,20 +1,24 @@
 package com.nhnacademy.mart;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BuyList {
 
     private final ArrayList<Item> items = new ArrayList<>();
 
-    // TODO add 메서드 생성
     public void add(String addList){
         StringTokenizer st = new StringTokenizer(addList);
-        while(!st.hasMoreTokens()){ // 다음 토큰이 없다면
+        while(st.hasMoreTokens()){ // 다음 토큰이 없다면
             String name = st.nextToken();
             int amount = Integer.valueOf(st.nextToken());
             items.add(new Item(name, amount));
         }
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
     }
 
 
@@ -25,6 +29,14 @@ public class BuyList {
         public Item(String name, int amount){
             this.name = name;
             this.amount = amount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAmount() {
+            return amount;
         }
     }
 }
