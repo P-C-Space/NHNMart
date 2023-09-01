006b3b7be8b3fcb4d613e577028ef23ea9b21566
jeongwoo <sun62073812@gmail.com>
2023-09-01 16:51:32 +0900

    진열대에서 삭제 구현

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/FoodStand.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -10,8 +10,19 @@ public class FoodStand {
         foods.add(food);
     }
 
-    public void delete(Food food){
-        foods.remove(food);
+    public Food delete(String name){
+        int index = -1;
+        for(int i = 0;i < foods.size();i++){
+            if(foods.get(i).getName().equals(name)){
+                 index = i;
+                 break;
+            }
+        }
+
+        return foods.get(index);
     }
 
+    public ArrayList<Food> getFoods() {
+        return foods;
+    }
 }
