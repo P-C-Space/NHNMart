d9d1f6741719de84b3f6b6956e468b287566f563
jeongwoo <sun62073812@gmail.com>
2023-09-02 11:41:59 +0900

    FoodStand Logger 작성

--- nhn-mart-문제/src/main/java/com/nhnacademy/mart/FoodStand.java
+++ nhn-mart-문제/src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.mart;
 
 import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FoodStand {
+    private static final Logger logger = LoggerFactory.getLogger(Food.class);
 
     private final ArrayList<Food> foods = new ArrayList<>();
 
@@ -15,11 +18,17 @@ public class FoodStand {
         for(int i = 0;i < foods.size();i++){
             if(foods.get(i).getName().equals(name)){
                  index = i;
+
                  break;
             }
         }
+        if(index == -1){
+            logger.warn("존재 하지 않는 품목 : " + name);
+            throw new IllegalArgumentException("존재하지 않는 품목입니다.");
+        }
 
-        return foods.get(index);
+        logger.info("품목 삭제 성공 : " + name);
+        return foods.remove(index);
     }
 
     public ArrayList<Food> getFoods() {
